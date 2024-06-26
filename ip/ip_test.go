4c1050c47453d024dba8e789c8cfda468059e611
mitsu3s <mitsu3s.dev@gmail.com>
2024-06-27 04:07:17 +0900

    Fix ip_test.go

--- ip/ip_test.go
+++ ip/ip_test.go
@@ -9,6 +9,37 @@ import (
 
 func TestCreateIPHeader(t *testing.T) {
 	t.Parallel()
+
+	testcases := []IPHeader{
+		{
+			Version:  4,
+			IHL:      5,
+			TOS:      0,
+			Length:   20,
+			ID:       0,
+			Flags:    0,
+			FragOff:  0,
+			TTL:      64,
+			Protocol: 6,
+			Checksum: 0,
+			SrcIP:    net.ParseIP("192.168.10.1"),
+			DstIP:    net.ParseIP("192.168.10.10"),
+		},
+		// {
+		// 	Version:  4,
+		// 	IHL:      5,
+		// 	TOS:      0,
+		// 	Length:   20,
+		// 	ID:       0,
+		// 	Flags:    0,
+		// 	FragOff:  0,
+		// 	TTL:      64,
+		// 	Protocol: 6,
+		// 	Checksum: 0,
+		// 	SrcIP:    net.ParseIP("192.168.10.1"),
+		// 	DstIP:    net.ParseIP("192.168.10.10"),
+		// },
+	}
 	version := uint8(4)
 	ihl := uint8(5)
 	tos := uint8(0)
@@ -24,16 +55,18 @@ func TestCreateIPHeader(t *testing.T) {
 
 	ip := CreateIPHeader(version, ihl, tos, length, id, flags, fragOff, ttl, protocol, checksum, src, dst)
 
-	assert.Equal(t, ip.Version, uint8(4))
-	assert.Equal(t, ip.IHL, uint8(5))
-	assert.Equal(t, ip.TOS, uint8(0))
-	assert.Equal(t, ip.Length, uint16(20))
-	assert.Equal(t, ip.ID, uint16(0))
-	assert.Equal(t, ip.Flags, uint8(0))
-	assert.Equal(t, ip.FragOff, uint16(0))
-	assert.Equal(t, ip.TTL, uint8(64))
-	assert.Equal(t, ip.Protocol, uint8(6))
-	assert.Equal(t, ip.Checksum, uint16(0))
-	assert.Equal(t, ip.SrcIP.String(), "192.168.10.1")
-	assert.Equal(t, ip.DstIP.String(), "192.168.10.10")
+	for i, testcase := range testcases {
+		assert.Equal(t, ip.Version, testcase.Version, "Testcase %d", i)
+		assert.Equal(t, ip.IHL, testcase.IHL, "Testcase %d", i)
+		assert.Equal(t, ip.TOS, testcase.TOS, "Testcase %d", i)
+		assert.Equal(t, ip.Length, testcase.Length, "Testcase %d", i)
+		assert.Equal(t, ip.ID, testcase.ID, "Testcase %d", i)
+		assert.Equal(t, ip.Flags, testcase.Flags, "Testcase %d", i)
+		assert.Equal(t, ip.FragOff, testcase.FragOff, "Testcase %d", i)
+		assert.Equal(t, ip.TTL, testcase.TTL, "Testcase %d", i)
+		assert.Equal(t, ip.Protocol, testcase.Protocol, "Testcase %d", i)
+		assert.Equal(t, ip.Checksum, testcase.Checksum, "Testcase %d", i)
+		assert.Equal(t, ip.SrcIP.String(), testcase.SrcIP.String(), "Testcase %d", i)
+		assert.Equal(t, ip.DstIP.String(), testcase.DstIP.String(), "Testcase %d", i)
+	}
 }
